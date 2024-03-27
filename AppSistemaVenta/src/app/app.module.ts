3aeb1f2d11b0d9491681cb74657976d53fe7a8c8
EzeAlarcon <ezequielalarconmejia@gmail.com>
2024-03-27 16:10:43 -0500

    Se Agrega Carpeta con Angular ser frontend

---
+++ AppSistemaVenta/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './Components/login/login.component';
+import { LayoutComponent } from './Components/layout/layout.component';
+
+import { SharedModule } from './Reutilizable/shared/shared.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    LayoutComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    SharedModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
